07ceba0433440f9f1cb5e097794ac71ea7d81b62
Andrey Kidin <andy87@users.noreply.github.com>
2024-06-12 02:08:32 +0300

    Update my.php up

--- app/views/profile/my.php
+++ app/views/profile/my.php
@@ -7,28 +7,31 @@ use resources\profile\ProfileMeResources;
  * @var ProfileMeResources $R
  */
 
+$userProfile = $R->userProfile;
+$friendProfileList = $R->friendProfileList;
+
 ?>
 
 <div class="block__profile">
 
     <div class="b_profile--layer __me">
 
-        <h1 class="b_profile--nickname"><?= $R->userProfile->name ?></h1>
+        <h1 class="b_profile--nickname"><?= $userProfile->name ?></h1>
 
-        <img class="b_profile--avatar" src="<?= $R->userProfile->avatar ?>" alt="<?= $R->userProfile->name ?>">
+        <img class="b_profile--avatar" src="<?= $userProfile->avatar ?>" alt="<?= $userProfile->name ?>">
 
         <div class="block__counters">
             <div class="b_counter--item">
                 <span class="b_counter--label">followers</span>
-                <span class="b_counter--value"><?= $R->userProfile->followers ?></span>
+                <span class="b_counter--value"><?= $userProfile->followers ?></span>
             </div>
             <div class="b_counter--item">
                 <span class="b_counter--label">subscribers</span>
-                <span class="b_counter--value"><?= $R->userProfile->subscribers ?></span>
+                <span class="b_counter--value"><?= $userProfile->subscribers ?></span>
             </div>
             <div class="b_counter--item">
                 <span class="b_counter--label">streams</span>
-                <span class="b_counter--value"><?= $R->userProfile->streams ?></span>
+                <span class="b_counter--value"><?= $userProfile->streams ?></span>
             </div>
         </div>
 
@@ -39,7 +42,7 @@ use resources\profile\ProfileMeResources;
         <h2 class="b_profile--header">Friends</h2>
 
         <div class="b_profile--friends">
-            <?php foreach ($R->friendProfileList as $friendProfile): ?>
+            <?php foreach ($friendProfileList as $friendProfile): ?>
                 <?= $this->render(FriendProfile::TEMPLATE, ['friendProfile' => $friendProfile]) ?>
             <?php endforeach; ?>
         </div>
